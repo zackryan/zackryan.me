30ed52f93c4a0e4034734fa5aff6d228cfcd5f48
Zack Ryan <zackryan@Zacks-MacBook-Pro.local>
2016-07-30 17:03:01 -0400

    enable hot reloading

--- server/index.js
+++ server/index.js
@@ -4,11 +4,18 @@ import path from "path";
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev'
 
 let app = express();
+const compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler,{
+  hot:true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
 });
